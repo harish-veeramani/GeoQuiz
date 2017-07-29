16ca0be90af6868aaa27609784891e187748caec
Harish Veeramani <hdv987@gmail.com>
2017-07-29 14:23:22 -0500

    Added back button, missing image files, and strings.

--- app/src/main/java/com/harish/bignerdranch/geoquiz/QuizActivity.java
+++ app/src/main/java/com/harish/bignerdranch/geoquiz/QuizActivity.java
@@ -1,21 +1,23 @@
 package com.harish.bignerdranch.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import static android.widget.Toast.makeText;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private ImageButton mBackButton;
     private TextView mQuestionTextView;
-    private int mQuestionIndex = 0;
+    private static int mQuestionIndex = 0;
+
+    private static final String TAG = "QuizActivity";
 
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_asia, true),
@@ -29,6 +31,7 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        Log.d(TAG, "OnCreate()");
 
         mFalseButton = (Button) findViewById(R.id.false_button);
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -36,18 +39,14 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, -5);
-                toast.show();
+                checkAnswer(false);
             }
         });
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 0);
-                toast.show();
+                checkAnswer(true);
             }
         });
 
@@ -62,10 +61,59 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+        mBackButton = (ImageButton) findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mQuestionIndex == 0) mQuestionIndex = mQuestionBank.length - 1;
+                else mQuestionIndex = mQuestionIndex - 1;
+                updateQuestion();
+            }
+        });
     }
 
     private void updateQuestion(){
         int question = mQuestionBank[mQuestionIndex].getTextResId();
         mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPress) {
+        boolean answer = mQuestionBank[mQuestionIndex].isAnswer();
+        if (userPress == answer) {
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
     }
 }
